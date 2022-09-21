a22265f1d7ed64d862b7c8d85d0e72c9f700ef38
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-09-21 08:21:25 +0200

    style: adding styling to header layout

--- src/component/header/header.tsx
+++ src/component/header/header.tsx
@@ -1,32 +1,50 @@
+import { useState } from 'react'
 import { FaPen } from 'react-icons/fa'
+import { BsMoon } from 'react-icons/bs'
+import './header.css'
+import '../../App.css'
 
 export function Header() {
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <header>
+    <header className="header">
       <h1 className="header__logo">Boruto</h1>
 
-      <aside className=" aside">
-        <button className="aside__write--button">
-          <FaPen /> Write
+      <aside className="aside">
+        <a className="aside__write--button">
+          <FaPen className="pen" /> Write
+        </a>
+
+        <button aria-label="dark mode" className="aside__theme--button">
+          <BsMoon />
         </button>
 
-        <button className="aside__theme--button"></button>
+        <img
+          onClick={() => setIsOpen(true)}
+          src="https://hashnode.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1659089761812%2FfsOct5gl6.png&w=1920&q=75"
+          alt="profile"
+          className="aside__profile"
+        />
 
-        <div className="aside__wrapper">
-          <img
-            className="aside__wrapper--profile"
-            src="https://hashnode.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1659089761812%2FfsOct5gl6.png&w=1920&q=75"
-            alt="profile"
-          />
-          <h2 className="aside__wrapper--info">
-            Sign up or log in to your Boruto account.
-          </h2>
-          <p className="aside__wrapper--text">Takes less than a few seconds.</p>
-          <div className="aside__wrapper--buttons">
-            <button>Sign Up</button>
-            <button>Login</button>
+        {isOpen === true && (
+          <div className="aside__wrapper">
+            <img
+              className="aside__wrapper--profile"
+              src="https://hashnode.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1659089761812%2FfsOct5gl6.png&w=1920&q=75"
+              alt="profile"
+            />
+            <h2 className="aside__wrapper--info">
+              Sign up or log in to your Boruto account.
+            </h2>
+            <p className="aside__wrapper--text">
+              Takes less than a few seconds.
+            </p>
+            <div className="aside__wrapper--buttons">
+              <button>Sign up</button>
+              <button>Log in</button>
+            </div>
           </div>
-        </div>
+        )}
       </aside>
     </header>
   )
