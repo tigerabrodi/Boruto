28096f9ea73c0fb0337dc92ce84fc9de77c47bf6
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-09-21 14:20:55 +0200

    feat: create login layout

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,3 +1,43 @@
+import { useState } from 'react'
+import { FaEye, FaEyeSlash } from 'react-icons/fa'
+
+import './login.css'
+
 export function LogIn() {
-  return <div>login</div>
+  const [passwordShown, setPasswordShown] = useState(false)
+  const togglePassword = (event: { preventDefault: () => void }) => {
+    event.preventDefault()
+    setPasswordShown(!passwordShown)
+  }
+  return (
+    <div className="signin">
+      <form className="form">
+        <h2>Sign In</h2>
+
+        <div className="form__wrapper">
+          <label htmlFor="Email">Email</label>
+          <input type="text" name="Name" id="Email" placeholder="" />
+        </div>
+
+        <div className="form__wrapper">
+          <label htmlFor="Password">Password</label>
+          <div className="form__wrapper--wrap">
+            <input
+              type={passwordShown ? 'text' : 'password'}
+              name="password"
+              id=" password"
+            />{' '}
+            <button onClick={togglePassword}>
+              {' '}
+              {passwordShown ? <FaEyeSlash /> : <FaEye />}
+            </button>
+          </div>
+        </div>
+
+        <button type="submit" className="form__button">
+          Sign in
+        </button>
+      </form>
+    </div>
+  )
 }
