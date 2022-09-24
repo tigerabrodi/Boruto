4c3e6e5965a71c0f2d9644b64f13af0e437c9ede
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-09-24 19:53:32 +0200

    feat: user is able to sign in

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -2,21 +2,50 @@ import { useState } from 'react'
 import { FaEye, FaEyeSlash } from 'react-icons/fa'
 
 import './login.css'
+import { useFormState } from '../../hooks/useFormState'
+import { useSignInWithEmailAndPassword } from '../../hooks/useSignInWithEmailAndPassword'
 
 export function LogIn() {
   const [passwordShown, setPasswordShown] = useState(false)
+
+  const { isSignInError, signInWithEmailAndPassword } =
+    useSignInWithEmailAndPassword()
+
+  const {
+    handleChange,
+    formState: { password, email },
+  } = useFormState({
+    password: '',
+    email: '',
+  })
+
   const togglePassword = (event: { preventDefault: () => void }) => {
     event.preventDefault()
     setPasswordShown(!passwordShown)
   }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    signInWithEmailAndPassword(email, password)
+  }
+
   return (
     <div className="signin">
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <h2>Sign In</h2>
 
         <div className="form__wrapper">
-          <label htmlFor="Email">Email</label>
-          <input type="text" name="Name" id="Email" placeholder="" />
+          <label htmlFor="email">Email</label>
+
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={handleChange}
+            aria-required="true"
+            value={email}
+          />
         </div>
 
         <div className="form__wrapper">
@@ -26,9 +55,11 @@ export function LogIn() {
               type={passwordShown ? 'text' : 'password'}
               name="password"
               id=" password"
-            />{' '}
+              onChange={handleChange}
+              aria-required="true"
+              value={password}
+            />
             <button onClick={togglePassword}>
-              {' '}
               {passwordShown ? <FaEyeSlash /> : <FaEye />}
             </button>
           </div>
@@ -37,6 +68,12 @@ export function LogIn() {
         <button type="submit" className="form__button">
           Sign in
         </button>
+
+        {isSignInError && (
+          <p className="alert danger center " role="alert">
+            Password or email is invalid.
+          </p>
+        )}
       </form>
     </div>
   )
