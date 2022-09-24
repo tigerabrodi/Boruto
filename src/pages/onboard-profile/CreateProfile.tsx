8afaa0c5b46d81d45fb2b5eba72b73b9c917b07c
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-09-24 15:24:04 +0200

    feat: create onboard profile layout and its fuctionality

--- src/pages/onboard-profile/CreateProfile.tsx
+++ src/pages/onboard-profile/CreateProfile.tsx
@@ -3,69 +3,91 @@ import './createprofile.css'
 import { IoCameraOutline } from 'react-icons/io5'
 import { useRef, useState } from 'react'
 import { firebaseDb, firebaseAuth, firebaseStorage } from '../../lib/firebase'
-import {
-  addDoc,
-  collection,
-  serverTimestamp,
-  updateDoc,
-  doc,
-} from 'firebase/firestore'
+import { updateDoc, doc } from 'firebase/firestore'
 import { getDownloadURL, ref, uploadString } from 'firebase/storage'
 import { useNavigate } from 'react-router-dom'
-import { updateProfile } from 'firebase/auth'
+import { useFormState } from '../../hooks/useFormState'
 
 export function CreateProfile() {
   const filePickerRef = useRef<any>(null)
-
   const [selectedFile, setSelectedFile] = useState<any>(null)
   const [loading, setLoading] = useState(false)
 
-  const [fullName, setFullName] = useState('')
-  const [username, setUsername] = useState('')
-  const [biography, setBiography] = useState('')
-  const [error, setError] = useState('')
+  const [isNameError, setIsNameError] = useState(false)
+  const [isAgeError, setIsAgeError] = useState(false)
+  const [isBioError, setIsBioError] = useState(false)
 
   const navigate = useNavigate()
 
-  const createAccount = async () => {
-    if (loading) return
-    setLoading(true)
+  const {
+    handleChange,
+    formState: { name, age, bio },
+  } = useFormState({
+    name: '',
+    age: '',
+    bio: '',
+  })
 
-    if (selectedFile === null) {
-      setError('Please select a profile picture')
-    } else if (username === '') {
-      setError('Please enter a username')
-    } else if (fullName === '') {
-      setError('Please enter your full name')
-    } else {
-      const documentReference = await addDoc(collection(firebaseDb, 'users'), {
-        name: fullName,
-        username: username,
-        biography: biography,
-        userId: firebaseAuth.currentUser?.uid,
-        timestamp: serverTimestamp(),
-      })
+  const isAnyFieldEmpty = !name.length || !age.length || !bio.length
+
+  const canUserSignUp = () => {
+    const isNameValueEmpty = name.length <= 0
+    if (isNameValueEmpty) {
+      setIsNameError(true)
+      return setTimeout(() => {
+        setIsNameError(false)
+      }, 3000)
+    }
+
+    const isAgeEmpty = name.length <= 0
+    if (isAgeEmpty) {
+      setIsAgeError(true)
+      return setTimeout(() => {
+        setIsAgeError(false)
+      }, 3000)
+    }
+
+    const isBioEmpty = name.length <= 0
+    if (isBioEmpty) {
+      setIsBioError(true)
+      return setTimeout(() => {
+        setIsBioError(false)
+      }, 3000)
+    }
+    return true
+  }
+
+  const handleSubmit = async (event: { preventDefault: () => void }) => {
+    event.preventDefault()
+
+    if (canUserSignUp() === true) {
+      if (loading) return setLoading(true)
 
       const imageReference = ref(
         firebaseStorage,
-        `users/${documentReference.id}/image`
+        `avatars/${firebaseAuth.currentUser!.uid}/image`
       )
 
       await uploadString(imageReference, selectedFile, 'data_url').then(
         async () => {
           const downloadURL = await getDownloadURL(imageReference)
 
-          await updateDoc(doc(firebaseDb, `users/${documentReference.id}`), {
-            image: downloadURL,
-          })
-          setError(' ')
+          await updateDoc(
+            doc(firebaseDb, `users/${firebaseAuth.currentUser!.uid}`),
+            {
+              age: age,
+              bio: bio,
+              fullname: name,
+              avatarUrl: downloadURL,
+            }
+          )
         }
       )
-    }
 
-    setLoading(false)
-    setSelectedFile(null)
-    navigate('/')
+      setLoading(false)
+      setSelectedFile(null)
+      navigate('/')
+    }
   }
 
   const addImageToPost = (e: any) => {
@@ -82,10 +104,8 @@ export function CreateProfile() {
 
   return (
     <div className="signup">
-      <div className="form">
-        {error && <p>{error}</p>}
+      <div className="form" onSubmit={handleSubmit}>
         <h2>Create your account</h2>
-
         {selectedFile ? (
           <div
             className="form__image--profile"
@@ -117,40 +137,65 @@ export function CreateProfile() {
           </div>
         )}
 
-        <div className="form__wrapper">
-          <label htmlFor="Full Name">Full Name</label>
-          <input
-            onChange={(e) => setFullName(e.target.value)}
-            type="text"
-            name="Full Name"
-            id="Full Name"
-          />
-        </div>
+        <div className="form__wrapper flex">
+          <div className="form__wrapper--aside">
+            <label htmlFor="Full Name">Full Name</label>
+            <input
+              type="text"
+              name="name"
+              id="Full Name"
+              onChange={handleChange}
+              value={name}
+              aria-invalid={isNameError ? 'true' : 'false'}
+            />
+            {isNameError && (
+              <p className="alert danger" role="alert">
+                Please enter your full name
+              </p>
+            )}
+          </div>
+          <div className="form__wrapper--aside ">
+            <label htmlFor="Age">Age</label>
 
-        <div className="form__wrapper ">
-          <label htmlFor="Username">Pick a username</label>
-          <input
-            onChange={(e) => setUsername(e.target.value)}
-            name="text"
-            id="Username"
-          />
+            <input
+              type="number"
+              name="age"
+              id="Age"
+              onChange={handleChange}
+              value={age}
+              aria-invalid={isAgeError ? 'true' : 'false'}
+            />
+            {isAgeError && (
+              <p className="alert danger" role="alert">
+                Please enter your age
+              </p>
+            )}
+          </div>
         </div>
 
         <div className="form__wrapper">
           <label htmlFor="Biography">Tell us about what you do:</label>
+
           <textarea
-            onChange={(e) => setBiography(e.target.value)}
-            name="text"
+            name="bio"
             id="Biography"
+            onChange={handleChange}
+            value={bio}
+            aria-invalid={isBioError ? 'true' : 'false'}
           />
+          {isBioError && (
+            <p className="alert danger" role="alert">
+              please enter about yourself
+            </p>
+          )}
         </div>
         <button
-          onClick={createAccount}
           type="submit"
           className="form__button"
-          disabled={loading}
+          disabled={isAnyFieldEmpty || selectedFile == null}
+          onClick={handleSubmit}
         >
-          {loading === true ? 'Next...' : 'Next'}
+          {loading ? 'Done...' : 'Done'}
         </button>
       </div>
     </div>
