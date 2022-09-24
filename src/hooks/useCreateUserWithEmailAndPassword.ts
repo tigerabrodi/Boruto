a2e0bc838a546262bf8d8c9fa2a8a061b2b98eba
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-09-24 10:34:23 +0200

    feat: create costum hook for sign up page

---
+++ src/hooks/useCreateUserWithEmailAndPassword.ts
@@ -0,0 +1,63 @@
+import * as React from 'react'
+import { doc, serverTimestamp, writeBatch } from '@firebase/firestore'
+import { createUserWithEmailAndPassword as createUserWithEmailAndPasswordAuth } from '@firebase/auth'
+import { firebaseAuth, firebaseDb } from '../lib/firebase'
+import { FirebaseError } from '@firebase/util'
+// import toast from 'react-hot-toast'
+
+export const useCreateUserWithEmailAndPassword = () => {
+  const [signUpError, setSignUpError] = React.useState<FirebaseError | null>(
+    null
+  )
+
+  const batch = writeBatch(firebaseDb)
+
+  const createUserWithEmailAndPassword = async (
+    email: string,
+    password: string,
+    username: string
+  ) => {
+    try {
+      const user = await createUserWithEmailAndPasswordAuth(
+        firebaseAuth,
+        email,
+        password
+      )
+
+      const userRef = doc(firebaseDb, `users/${user.user.uid}`)
+
+      batch.set(userRef, {
+        username,
+        email,
+        fullname: '',
+        age: '',
+        bio: '',
+        avatarUrl: '',
+        likeCount: 0,
+        postCount: 0,
+        createdAt: serverTimestamp(),
+        uid: user.user.uid,
+      })
+
+      const usernameRef = doc(firebaseDb, `usernames/${username}`)
+      batch.set(usernameRef, {
+        uid: user.user?.uid,
+      })
+
+      await batch.commit()
+
+      //   router.push(`/${username}/edit`)
+      //   toast.success('You successfully created your account.')
+    } catch (error) {
+      setSignUpError(error as FirebaseError)
+      setTimeout(() => {
+        setSignUpError(null)
+      }, 3000)
+    }
+  }
+
+  return {
+    createUserWithEmailAndPassword,
+    signUpError,
+  }
+}
