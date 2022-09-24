42c23eb7e10deba67bad6d9a6c095d865384e1fa
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-09-24 15:22:53 +0200

    feat: create file for types

---
+++ src/lib/types.ts
@@ -0,0 +1,53 @@
+import { User } from '@firebase/auth'
+import { FieldValue, Timestamp } from '@firebase/firestore'
+
+export type DateType = FieldValue | Timestamp | number
+
+export type CreatedAt = { createdAt: DateType }
+
+export type UserData = {
+  username: string | null
+  user: User | null | undefined
+}
+
+export type UserProfile = {
+  username: string
+  email: string
+  fullname: string
+  age: string
+  bio: string
+  avatarUrl: string
+  likeCount: number
+  postCount: number
+  createdAt: DateType
+  uid: string
+} & CreatedAt
+
+export type Post = {
+  title: string
+  body: string
+  commentsCount: number
+  likeCount: number
+  createdAt: DateType
+  uid: string
+  imageUrl: string
+  slug: string
+  readingTime: string
+  authorUsername: string
+  authorAvatarUrl: string
+  authorFullname: string
+} & CreatedAt
+
+export type Comment = {
+  text: string
+  claplikeount: number
+  uid: string
+  authorUsername: string
+  authorAvatarUrl: string
+  authorFullname: string
+  id: string
+} & CreatedAt
+
+export type Styleable = {
+  className?: string
+}
