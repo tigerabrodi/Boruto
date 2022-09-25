ac859d7ef2750169dda252bccc121544f277e4fe
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-09-25 13:47:31 +0200

    set menu to users profile

--- src/components/header/authenticated/Authenticated.tsx
+++ src/components/header/authenticated/Authenticated.tsx
@@ -1,13 +1,31 @@
-import { firebaseAuth } from '../../../lib/firebase'
+import { firebaseAuth, firebaseDb } from '../../../lib/firebase'
 import './authenticated.css'
 import { FiUser, FiLogOut, FiBookmark } from 'react-icons/fi'
 import { useLoadingStore } from '../../../lib/store'
 import { useNavigate } from 'react-router-dom'
 import toast from 'react-hot-toast'
+import { useEffect, useState } from 'react'
+import { collection, CollectionReference, onSnapshot } from 'firebase/firestore'
+import { useAuthContext } from '../../../context/AuthContext'
+
+type UserType = {
+  fullname: string
+  avatarUrl: string
+  username: string
+  id: string
+}
 
 export function Authenticated() {
+  const [profile, setProfile] = useState<UserType[]>([])
   const { setStatus } = useLoadingStore()
   const navigate = useNavigate()
+
+  const { user } = useAuthContext()
+
+  const userCollectionReference = collection(
+    firebaseDb,
+    'users'
+  ) as CollectionReference<UserType>
 
   const handleSignOut = async () => {
     setStatus('loading')
@@ -17,28 +35,47 @@ export function Authenticated() {
     setStatus('success')
   }
 
+  useEffect(() => {
+    const getProfile = () => {
+      onSnapshot(userCollectionReference, (snapshot) => {
+        setProfile(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+      })
+    }
+    getProfile()
+  }, [])
+
   return (
-    <div className="authenticated">
-      <div className="authenticated__wrapper">
-        <img
-          src="https://hashnode.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1659089761812%2FfsOct5gl6.png&w=1920&q=75"
-          alt="profile"
-        />
-        <div className="authenticated__wrapper--info">
-          <p>Full Name</p>
-          <p>@username</p>
-        </div>
-      </div>
-      <button className="authenticated__profile--button">
-        <FiUser className="icon" /> My Profile
-      </button>
-      <button className="authenticated__bookmarks--button">
-        <FiBookmark className="icon" />
-        My Bookmarks
-      </button>
-      <button onClick={handleSignOut} className="authenticated__logout--button">
-        <FiLogOut className="icon" /> Log out
-      </button>
-    </div>
+    <>
+      {profile.map(({ username, avatarUrl, fullname, id }) => {
+        return (
+          <div key={id} className="authenticated">
+            {user?.uid === id && (
+              <>
+                <div className="authenticated__wrapper">
+                  <img src={avatarUrl} alt="profile" />
+                  <div className="authenticated__wrapper--info">
+                    <p>{fullname}</p>
+                    <p>@{username}</p>
+                  </div>
+                </div>
+                <button className="authenticated__profile--button">
+                  <FiUser className="icon" /> My Profile
+                </button>
+                <button className="authenticated__bookmarks--button">
+                  <FiBookmark className="icon" />
+                  My Bookmarks
+                </button>
+                <button
+                  onClick={handleSignOut}
+                  className="authenticated__logout--button"
+                >
+                  <FiLogOut className="icon" /> Log out
+                </button>{' '}
+              </>
+            )}
+          </div>
+        )
+      })}
+    </>
   )
 }
