340e43035e1c14e206b52489073a7b59cd9914a0
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-02 12:57:54 +0200

    remove location logic

--- src/components/header/avatar/avatar.tsx
+++ src/components/header/avatar/avatar.tsx
@@ -2,7 +2,6 @@ import type { CollectionReference } from 'firebase/firestore'
 
 import { collection, onSnapshot } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
 
 import { useAuthContext } from '../../../context/AuthContext'
 import { useHeaderMenuContext } from '../../../context/MenuContext'
@@ -21,15 +20,8 @@ export function Avatar() {
   const { setIsMenuOpen } = useHeaderMenuContext()
   const { user } = useAuthContext()
 
-  const location = useLocation()
-
   const defaultAvatar =
     'https://hashnode.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1659089761812%2FfsOct5gl6.png&w=1920&q=75'
-
-  const isHome =
-    location.pathname === '/' ||
-    location.pathname === '/create/post' ||
-    location.pathname === '/profile'
 
   const userCollectionReference = collection(
     firebaseDb,
@@ -46,35 +38,33 @@ export function Avatar() {
   }, [])
   return (
     <>
-      {isHome && (
-        <>
-          {user?.uid ? (
-            <>
-              {profile.map(({ avatarUrl, id }) => {
-                return (
-                  <div key={id}>
-                    {user?.uid === id && (
-                      <img
-                        onClick={() => setIsMenuOpen(true)}
-                        src={avatarUrl}
-                        alt="profile"
-                        className="header__aside--avatar"
-                      />
-                    )}
-                  </div>
-                )
-              })}
-            </>
-          ) : (
-            <img
-              alt="no profile"
-              className="header__aside--avatar"
-              onClick={() => setIsMenuOpen(true)}
-              src={defaultAvatar}
-            />
-          )}
-        </>
-      )}
+      <>
+        {user?.uid ? (
+          <>
+            {profile.map(({ avatarUrl, id }) => {
+              return (
+                <div key={id}>
+                  {user?.uid === id && (
+                    <img
+                      onClick={() => setIsMenuOpen(true)}
+                      src={avatarUrl}
+                      alt="profile"
+                      className="header__aside--avatar"
+                    />
+                  )}
+                </div>
+              )
+            })}
+          </>
+        ) : (
+          <img
+            alt="no profile"
+            className="header__aside--avatar"
+            onClick={() => setIsMenuOpen(true)}
+            src={defaultAvatar}
+          />
+        )}
+      </>
     </>
   )
 }
