5cef777d15b4ad9a4f9431bc305c5d1dc341030a
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-03 20:29:57 +0200

    commenting out, due to type problemes

--- src/pages/write/preview.tsx
+++ src/pages/write/preview.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import ReactMarkdown from 'react-markdown'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+// import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+// import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 import rehypeRaw from 'rehype-raw'
 import remarkGfm from 'remark-gfm'
 
@@ -16,24 +16,24 @@ export function Preview({ textField }: PreviewProps) {
       className="write__container--preview"
       remarkPlugins={[remarkGfm]}
       rehypePlugins={[rehypeRaw]}
-      components={{
-        code({ node, inline, className, children, ...props }) {
-          const match = /language-(\w+)/.exec(className || '')
-          return !inline && match ? (
-            <SyntaxHighlighter
-              style={dracula}
-              children={String(children).replace(/\n$/, '')}
-              language={match[1]}
-              PreTag="div"
-              {...props}
-            />
-          ) : (
-            <code className={className} {...props}>
-              {children}
-            </code>
-          )
-        },
-      }}
+      // components={{
+      //   code({ node, inline, className, children, ...props }) {
+      //     const match = /language-(\w+)/.exec(className || '')
+      //     return !inline && match ? (
+      //       <SyntaxHighlighter
+      //         style={dracula}
+      //         children={String(children).replace(/\n$/, '')}
+      //         language={match[1]}
+      //         PreTag="div"
+      //         {...props}
+      //       />
+      //     ) : (
+      //       <code className={className} {...props}>
+      //         {children}
+      //       </code>
+      //     )
+      //   },
+      // }}
     />
   )
 }
