d4961e0673f29638a882900d58c4756a6148d686
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-06 18:48:09 +0200

    test: create a blog article

--- cypress/e2e/sign-in-create-article.cy.ts
+++ cypress/e2e/sign-in-create-article.cy.ts
@@ -1,16 +1,16 @@
-// const DEMO_COVER_IMAGE = '../fixtures/demo-cover-image.png'
+const DEMO_COVER_IMAGE = '../fixtures/demo-cover-image.png'
 
 const firstUser = {
   password: 'demonslayer123',
   email: 'borutouzumaki@gmail.com',
 }
 
-// const article = {
-//   readMin: 3,
-//   title: 'Meaning of Lorem Ipsum',
-//   subtitle: 'Lorem ipsum was purposefully designed to have no meaning',
-//   text: `What I find remarkable is that this text has been the industry's standard dummy text ever since some printer in the 1500s took a galley of type and scrambled it to make a type specimen book; it has survived not only four centuries of letter-by-letter resetting but even the leap into electronic typesetting, essentially unchanged except for an occasional 'ing' or 'y' thrown in. It's ironic that when the then-understood Latin was scrambled, it became as incomprehensible as Greek; the phrase 'it's Greek to me' and 'greeking' have common semantic roots!`,
-// }
+const article = {
+  readMin: '3',
+  title: 'Meaning of Lorem Ipsum',
+  subtitle: 'Lorem ipsum was purposefully designed to have no meaning',
+  text: `What I find remarkable is that this text has been the industry's standard dummy text ever since some printer in the 1500s took a galley of type and scrambled it to make a type specimen book; it has survived not only four centuries of letter-by-letter resetting but even the leap into electronic typesetting, essentially unchanged except for an occasional 'ing' or 'y' thrown in.`,
+}
 
 it('Should be able to sign in', () => {
   cy.visit('/')
@@ -30,9 +30,18 @@ it('Should be able to sign in', () => {
   cy.location('pathname').should('eq', '/')
 })
 
-// it('Should create a blog article', () => {
-//   // Redirected to "write article page"
-//   cy.get('[data-cy="write-button"]').click()
+it('Should create a blog article', () => {
+  // Redirected to "create article' page
+  cy.get('[data-cy="write-button"]').click()
 
-//   cy.get('[data-cy="write-file-input"]').click().attachFile(DEMO_COVER_IMAGE)
-// })
+  // Create blog article article
+  cy.get('[data-cy="write-file-input"]').attachFile(DEMO_COVER_IMAGE)
+  cy.get('[data-cy="write-read-min-input"]').type(article.readMin)
+  cy.get('[data-cy="write-title-textarea"]').type(article.title)
+  cy.get('[ data-cy="write-subtitle-button"]').click()
+  cy.get('[data-cy="write-subtitle-textarea"]').type(article.subtitle)
+  cy.get('[data-cy="write-text-textarea"]').type(article.text)
+
+  // Post blog article
+  cy.get('[data-cy="write-publish-button"]').click()
+})
