14b49f24e74133a623c87caa8c9f6b49a9e9c564
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-08 11:31:40 +0200

    delete article

---
+++ src/pages/article/buttons/editDelete/editDelete.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import { FiEdit3, FiX } from 'react-icons/fi'
+
+import { DeleteArticleModal } from '../../../../components/modals/DeleteArticleModal'
+import { useAuthContext } from '../../../../context/AuthContext'
+
+import '../../article.css'
+
+type EditDeleteProps = {
+  uid: string
+  articleId: string | undefined
+}
+
+export function EditDelete({ uid, articleId }: EditDeleteProps) {
+  const [openModal, setOpenModal] = useState(false)
+  const { user } = useAuthContext()
+  return (
+    <>
+      {openModal ? (
+        <DeleteArticleModal setOpenModal={setOpenModal} articleId={articleId} />
+      ) : (
+        ''
+      )}
+      {uid === user?.uid && (
+        <div className="card-wrapper-button">
+          <button className="article__edit" aria-label="Edit your blog article">
+            <FiEdit3 />
+          </button>
+          <button
+            onClick={() => setOpenModal(true)}
+            className="article__delete"
+            aria-label="Delete your blog article"
+          >
+            <FiX />
+          </button>
+        </div>
+      )}
+    </>
+  )
+}
