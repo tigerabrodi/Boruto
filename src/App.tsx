d64d9fa52bbd3f842616ecba2fd4ef456807d539
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-08 19:08:46 +0200

    create edit user component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { Routes, Route } from 'react-router-dom'
 import { AuthContextProvider } from './context/AuthContext'
 import { InfoModuleContextProvider } from './context/InfoModuleContext'
 import { HeaderMenuContextProvider } from './context/MenuContext'
-import { SkeletonContextProvider } from './context/SkeletonContext'
 import {
   Header,
   SignUp,
@@ -16,6 +15,7 @@ import {
   User,
   Article,
   Profile,
+  EditUser,
 } from './files'
 import { ToastOptions } from './styles/theme'
 
@@ -23,29 +23,28 @@ export function App() {
   return (
     <div className="app">
       <AuthContextProvider>
-        <SkeletonContextProvider>
-          <HeaderMenuContextProvider>
-            <Header />
-          </HeaderMenuContextProvider>
-          <LoadingSpinner />
-          <Toaster
-            position="top-center"
-            toastOptions={ToastOptions}
-            data-cy="toast-message"
-          />
-          <InfoModuleContextProvider>
-            <Routes>
-              <Route path="/" element={<Feed />} />
-              <Route path="/signin" element={<SignIn />} />
-              <Route path="/signup" element={<SignUp />} />
-              <Route path="/my-profile" element={<User />} />
-              <Route path="/profile/:profileID" element={<Profile />} />
-              <Route path="/create/post" element={<Write />} />
-              <Route path="/onboard/profile" element={<CreateProfile />} />
-              <Route path="/article/:id" element={<Article />} />
-            </Routes>
-          </InfoModuleContextProvider>
-        </SkeletonContextProvider>
+        <HeaderMenuContextProvider>
+          <Header />
+        </HeaderMenuContextProvider>
+        <LoadingSpinner />
+        <Toaster
+          position="top-center"
+          toastOptions={ToastOptions}
+          data-cy="toast-message"
+        />
+        <InfoModuleContextProvider>
+          <Routes>
+            <Route path="/" element={<Feed />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/my-profile" element={<User />} />
+            <Route path="/edit/my-profile" element={<EditUser />} />
+            <Route path="/profile/:profileID" element={<Profile />} />
+            <Route path="/create/post" element={<Write />} />
+            <Route path="/onboard/profile" element={<CreateProfile />} />
+            <Route path="/article/:id" element={<Article />} />
+          </Routes>
+        </InfoModuleContextProvider>
       </AuthContextProvider>
     </div>
   )
---
+++ src/pages/edit/editUser/EditUser.tsx
@@ -0,0 +1,9 @@
+import './editUser.css'
+
+export function EditUser() {
+  return (
+    <div className="container">
+      <form className="edit-profile"></form>
+    </div>
+  )
+}
