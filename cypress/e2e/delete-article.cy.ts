c964c3f28ec7ee054e6812e6cdd387075e24b552
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-08 14:43:19 +0200

    test: delete an article

---
+++ cypress/e2e/delete-article.cy.ts
@@ -0,0 +1,14 @@
+it('Should be able to delete an article', () => {
+  cy.visit('/')
+
+  // Gets redirected users profile
+  cy.get('[data-cy="authenticated-avatar"]').click()
+  cy.get('[data-cy="my-profile-link"]').click()
+
+  // Gets redirected to the article
+  cy.get('[data-cy="users-article"]').click()
+
+  // Delete article
+  cy.get('[data-cy="delete-article-button"]').click()
+  cy.get('[data-cy="delete-article-modal-button"]').click()
+})
